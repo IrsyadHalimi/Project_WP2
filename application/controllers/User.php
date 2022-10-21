58d7cffcb18d115d5e8ac69a00c9cc55dbbe6e9c
IrsyadHalimi <irsyadhalimi08@gmail.com>
2022-10-21 23:29:58 +0700

    ditambahkan templates dari bootstrap, belum dimodifikasi, login masih error

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,9 +4,11 @@ class User extends CI_Controller
 {
     public function index()
     {
-        $data['user'] = $this->db->get_where('user', ['email' =>
-        $this->session->userdata('email')])->row_array();
+        // $data['user'] = $this->db->get_where('user', ['email' =>
+        // $this->session->userdata('email')])->row_array();
 
         $this->load->view('user/index');
+        $this->load->view('templates/user_header');
+        $this->load->view('templates/user_footer');
     }
 }
--- application/views/auth/registrasi.php
+++ application/views/auth/registrasi.php
@@ -9,7 +9,7 @@
                         <div class="text-center">
                             <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                         </div>
-                        <form class="user" method="POST" action="<?= base_url("auth/registrasi") ?>">
+                        <form class="user" method="POST" action="<?= base_url("autentifikasi/registrasi") ?>">
                             <div class="form-group">
                                 <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Nama Lengkap" value="<?= set_value('name'); ?>">
                                 <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
@@ -36,7 +36,7 @@
                             <a class="small" href="forgot-password.html">Forgot Password?</a>
                         </div>
                         <div class="text-center">
-                            <a class="small" href="<?= base_url('auth/'); ?>">Already have an account? Login!</a>
+                            <a class="small" href="<?= base_url('autentifikasi/'); ?>">Already have an account? Login!</a>
                         </div>
                     </div>
                 </div>
